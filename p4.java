62d8695d6a2a983596dc1427aa822d64d1ed9cc9
shawwrr <shhaworkk@gmail.com>
2026-02-21 09:05:50 +0530

    Add files via upload
    This is the Java program to input two numbers from user and  print thier sum

---
+++ p4.java
@@ -0,0 +1,13 @@
+// to print the sum of to numbers from user
+import java.util.*;
+public class p4
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a+b;
+        System.out.println("the sum of numbers is"+sum);
+    }
+}
